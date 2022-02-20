0efc62e4770c4161f30ff0aadba4dff62dbf3035
Sadrac Fernández <sadfer.ing@gmail.com>
2022-02-20 11:51:58 -0300

    Fin sección 5: Expandiendo nuestras bases

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+  constructor(private dbzService: DbzService) {}
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    this.dbzService.agregarPersona(this.nuevo);
+    console.log(this.nuevo);
+    // this.onNuevoPersonaje.emit(this.nuevo);
+    this.nuevo = { nombre: '', poder: 0 };
+  }
+}
